deecf02da605d967fcc1ccc4ecba5a110fcdf87d
Dan Davis <daniel.davis@nih.gov>
2020-01-30 10:07:33 -0500

    Add a react app (not a React SSG)

---
+++ routines-react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Routines
+      </header>
+      <main className="App-main">
+        <ul className="routine-list">
+          <li>Morning</li>
+          <li>Bedtime</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
